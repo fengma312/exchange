5df3e757ac2a03c8d79ab82a95efda28853c00b7
mafeng <feng.ma@klickl.com>
2023-03-10 11:02:01 +0800

    a

--- Com.Bll/Src/ServiceCluster.cs
+++ Com.Bll/Src/ServiceCluster.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using Com.Bll.Models;
 using Com.Models.Db;
 using Com.Models.Enum;
@@ -14,6 +15,8 @@ public class ServiceCluster
     /// </summary>
     public readonly ServiceBase service_base;
 
+    private readonly ConcurrentDictionary<long, Cluster> cluster = new ConcurrentDictionary<long, Cluster>();
+
     /// <summary>
     /// 初始化
     /// </summary>
@@ -21,6 +24,13 @@ public class ServiceCluster
     public ServiceCluster(ServiceBase service_base)
     {
         this.service_base = service_base;
+        using (DbContextEF db = this.service_base.db_factory.CreateDbContext())
+        {
+            foreach (var item in db.Cluster.ToList())
+            {
+                this.cluster.TryAdd(item.cluster_id, item);
+            }
+        }
     }
 
     /// <summary>
@@ -32,10 +42,14 @@ public class ServiceCluster
     public string? GetClusterUrl(E_ServiceType type, long id)
     {
         long mark = id % 10;
-        using (DbContextEF db = this.service_base.db_factory.CreateDbContext())
+        foreach (var item in this.cluster.Values)
         {
-            return db.Cluster.FirstOrDefault(P => P.type == type && P.mark.Contains(mark.ToString()))?.url;
+            if (item.type == type && item.mark.Contains(mark.ToString()))
+            {
+                return item.url;
+            }
         }
+        return null;
     }
 
 
