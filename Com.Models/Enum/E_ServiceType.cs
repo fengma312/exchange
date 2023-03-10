a5745685c7c6fa946287104e6d43fd6c6efb0eba
mafeng <feng.ma@klickl.com>
2023-03-10 16:10:08 +0800

    a

--- Com.Models/Enum/E_ServiceType.cs
+++ Com.Models/Enum/E_ServiceType.cs
@@ -6,24 +6,28 @@ namespace Com.Models.Enum;
 public enum E_ServiceType
 {
     /// <summary>
-    /// 撮合服务
-    /// </summary>
-    match = 1,
-    /// <summary>
-    /// 交易服务
-    /// </summary>
-    trade = 2,
-    /// <summary>
     /// 管理员api服务
     /// </summary>
-    admin = 3,
-    /// <summary>
-    /// 账户服务
-    /// </summary>
-    account = 4,
+    admin = 1,
     /// <summary>
     /// 公开api服务
     /// </summary>
-    openapi = 5
+    openapi = 2,
+    /// <summary>
+    /// 账户服务
+    /// </summary>
+    account = 3,
+    /// <summary>
+    /// 交易服务
+    /// </summary>
+    trade = 4,
+    /// <summary>
+    /// 撮合服务
+    /// </summary>
+    match = 5,
+
+
+
+
 
 }
