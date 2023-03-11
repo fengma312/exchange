935a7c8fd005935445621097532e10ab4cc2e645
fengma_312 <fengma_312@126.com>
2023-03-11 18:23:47 +0800

    a

--- Com.Bll/Src/GrpcClientTrade.cs
+++ Com.Bll/Src/GrpcClientTrade.cs
@@ -20,6 +20,7 @@ public class GrpcClientTrade
     /// grpc客户端对象
     /// </summary>
     public readonly TradeGrpc.TradeGrpcClient client;
+    
     /// <summary>
     /// 初始化
     /// </summary>
@@ -35,11 +36,11 @@ public class GrpcClientTrade
     /// 1:一元调用:判断程序/网络是否启动
     /// </summary>
     /// <returns></returns>
-    public async Task<bool> Activity()
+    public async Task<bool> TradeActive()
     {
         try
         {
-            Google.Protobuf.WellKnownTypes.Empty res = await client.ActivityAsync(new Google.Protobuf.WellKnownTypes.Empty());
+            Google.Protobuf.WellKnownTypes.Empty res = await client.TradeActiveAsync(new Google.Protobuf.WellKnownTypes.Empty());
         }
         catch
         {
@@ -49,29 +50,57 @@ public class GrpcClientTrade
         return true;
     }
 
-    // /// <summary>
-    // /// 2:加载用户信息
-    // /// </summary>
-    // /// <param name="users_id"></param>
-    // /// <returns></returns>
-    // public async Task LoadUser(List<long> users_id)
-    // {
-    //     LoadUserReq req = new LoadUserReq();
-    //     req.UserId.AddRange(users_id);
-    //     await client.LoadUserAsync(req);
-    // }
+    /// <summary>
+    /// 2:加载用户信息
+    /// </summary>
+    /// <param name="market_id"></param>
+    /// <returns></returns>
+    public async Task<bool> TradeLoad(List<long> market_id)
+    {
+        TradeLoadReq req = new TradeLoadReq();
+        req.MarketId.AddRange(market_id);
+        TradeLoadRes res = await client.TradeLoadAsync(req);
+        return res.Success;
+    }
 
-    // /// <summary>
-    // /// 2:加载用户信息
-    // /// </summary>
-    // /// <param name="users_id"></param>
-    // /// <returns></returns>
-    // public async Task<GetUserRes> GetUser(List<long> users_id)
-    // {
-    //     GetUserReq req = new GetUserReq();
-    //     req.UserId.AddRange(users_id);
-    //     return await client.GetUserAsync(req);
-    // }
+    /// <summary>
+    /// 3:一元调用:开启交易服务
+    /// </summary>
+    /// <param name="market_id"></param>
+    /// <returns></returns>
+    public async Task<bool> TradeStart(List<long> market_id)
+    {
+        TradeStartReq req = new TradeStartReq();
+        req.MarketId.AddRange(market_id);
+        TradeStartRes res = await client.TradeStartAsync(req);
+        return res.Success;
+    }
+
+    /// <summary>
+    /// 4:一元调用:停止交易服务
+    /// </summary>
+    /// <param name="market_id"></param>
+    /// <returns></returns>
+    public async Task<bool> TradeStop(List<long> market_id)
+    {
+        TradeStopReq req = new TradeStopReq();
+        req.MarketId.AddRange(market_id);
+        TradeStopRes res = await client.TradeStopAsync(req);
+        return res.Success;
+    }
+
+    /// <summary>
+    /// 5:一元调用:交易对配置更改
+    /// </summary>
+    /// <param name="market_id"></param>
+    /// <returns></returns>
+    public async Task<bool> TradeChange(List<long> market_id)
+    {
+        TradeChangeReq req = new TradeChangeReq();
+        req.MarketId.AddRange(market_id);
+        TradeChangeRes res = await client.TradeChangeAsync(req);
+        return res.Success;
+    }
 
     /// <summary>
     /// 关闭
--- Com.Service/Com.Service.Trade/Src/GrpcServiceTrade.cs
+++ Com.Service/Com.Service.Trade/Src/GrpcServiceTrade.cs
@@ -1,4 +1,5 @@
 
+using Com.Models.Db;
 using Com.Models.Enum;
 using Com.Service.Trade.Models;
 using Grpc.Core;
@@ -17,37 +18,74 @@ public class GrpcServiceTrade : TradeGrpc.TradeGrpcBase
     /// <param name="request"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public override async Task<Google.Protobuf.WellKnownTypes.Empty> Activity(Google.Protobuf.WellKnownTypes.Empty request, ServerCallContext context)
+    public override async Task<Google.Protobuf.WellKnownTypes.Empty> TradeActive(Google.Protobuf.WellKnownTypes.Empty request, ServerCallContext context)
     {
         return await Task.FromResult(request);
     }
 
     /// <summary>
-    /// 2:一元调用:管理撮合交易对(开启,停止)
+    /// 2:一元调用:分配撮合交易对
     /// </summary>
     /// <param name="request"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public override async Task<MarketManageRes> MarketManage(MarketManageReq request, ServerCallContext context)
+    public override async Task<TradeLoadRes> TradeLoad(TradeLoadReq request, ServerCallContext context)
     {
-        MarketManageRes res = new MarketManageRes();
-        foreach (var item in request.Market)
+        TradeLoadRes res = new TradeLoadRes()
         {
-            if (FactoryTrade.instance.service.TryGetValue(item.Key, out TradeModel? model))
+            Success = true,
+        };
+        List<Market> markets = FactoryTrade.instance.service_list.service_market.GetMarketById(request.MarketId.ToList());
+        foreach (var item in markets)
+        {
+            TradeModel model = new TradeModel(item)
             {
-                if (item.Value == true && model.run == false)
-                {
-                    model.StartTrade();
-                }
-                if (item.Value == false && model.run == true)
-                {
-                    model.StopTrade();
-                }
-                res.Market.Add(new KeyValueGrpc()
-                {
-                    Key = item.Key,
-                    Value = true
-                });
+
+            };
+            FactoryTrade.instance.service.AddOrUpdate(item.market_id, model, (k, v) => { return model; });
+        }
+        return await Task.FromResult(res);
+    }
+
+    /// <summary>
+    /// 3:一元调用:开启交易服务
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public override async Task<TradeStartRes> TradeStart(TradeStartReq request, ServerCallContext context)
+    {
+        TradeStartRes res = new TradeStartRes()
+        {
+            Success = true,
+        };
+        foreach (var item in request.MarketId)
+        {
+            if (FactoryTrade.instance.service.TryGetValue(item, out TradeModel? model))
+            {
+                model.StartTrade();
+            }
+        }
+        return await Task.FromResult(res);
+    }
+
+    /// <summary>
+    /// 3:一元调用:开启交易服务
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public override async Task<TradeStopRes> TradeStop(TradeStopReq request, ServerCallContext context)
+    {
+        TradeStopRes res = new TradeStopRes()
+        {
+            Success = true,
+        };
+        foreach (var item in request.MarketId)
+        {
+            if (FactoryTrade.instance.service.TryGetValue(item, out TradeModel? model))
+            {
+                model.StopTrade();
             }
         }
         return await Task.FromResult(res);
@@ -59,137 +97,20 @@ public class GrpcServiceTrade : TradeGrpc.TradeGrpcBase
     /// <param name="request"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public override async Task<MarketConfigRes> MarketConfig(MarketConfigReq request, ServerCallContext context)
+    public override async Task<TradeChangeRes> TradeChange(TradeChangeReq request, ServerCallContext context)
     {
-        MarketConfigRes res = new MarketConfigRes();
-        foreach (var item in request.Market)
+        TradeChangeRes res = new TradeChangeRes()
         {
-            if (FactoryTrade.instance.service.TryGetValue(item.Key, out TradeModel? model))
+            Success = true,
+        };
+        foreach (var item in request.MarketId)
+        {
+            if (FactoryTrade.instance.service.TryGetValue(item, out TradeModel? model))
             {
                 model.ReloadConfig();
-                res.Market.Add(new KeyValueGrpc()
-                {
-                    Key = item.Key,
-                    Value = true
-                });
             }
         }
         return await Task.FromResult(res);
     }
-
-    /// <summary>
-    /// 2:一元调用:查询所分配的交易对
-    /// </summary>
-    /// <param name="request"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-    // public override async Task<SearchSymbolRes> SearchSymbol(SearchSymbolReq request, ServerCallContext context)
-    // {
-    //     SearchSymbolRes res = new SearchSymbolRes();
-    //     List<SymbolList> symbols = new List<SymbolList>();
-    //     // foreach (var item in FactoryTrade.instance.service)
-    //     // {
-    //     //     MarketType market_type = item.Value.info.market_type switch
-    //     //     {
-    //     //         E_MarketType.spot => MarketType.Spot,
-    //     //         _ => MarketType.Spot
-    //     //     };
-    //     //     symbols.Add(new SymbolList()
-    //     //     {
-    //     //         SymbolId = item.Key,
-    //     //         Symbol = item.Value.info.symbol,
-    //     //         MarketType = market_type,
-    //     //         Run = item.Value.run,
-    //     //     });
-    //     // }
-    //     res.SymbolLists.Add(symbols);
-    //     return await Task.FromResult<SearchSymbolRes>(res);
-    // }
-
-    // /// <summary>
-    // /// 3:一元调用:分配撮合交易对
-    // /// </summary>
-    // /// <param name="request"></param>
-    // /// <param name="context"></param>
-    // /// <returns></returns>
-    // public override async Task<InitMatchRes> InitMatch(InitMatchReq request, ServerCallContext context)
-    // {
-    //     InitMatchRes res = new InitMatchRes();
-    //     res.Success = FactoryTrade.instance.InitMatch(request.SymbolId);
-    //     return await Task.FromResult(res);
-    // }
-
-    // /// <summary>
-    // /// 4:一元调用:管理撮合交易对(开启,停止)
-    // /// </summary>
-    // /// <param name="request"></param>
-    // /// <param name="context"></param>
-    // /// <returns></returns>
-    // public override async Task<ManageSymbolRes> ManageSymbol(ManageSymbolReq request, ServerCallContext context)
-    // {
-    //     ManageSymbolRes res = new ManageSymbolRes();
-    //     if (request.Status)
-    //     {
-    //         res.Success = FactoryTrade.instance.Start(request.SymbolId);
-    //     }
-    //     else
-    //     {
-    //         res.Success = FactoryTrade.instance.Stop(request.SymbolId);
-    //     }
-    //     return await Task.FromResult(res);
-    // }
-
-    // /// <summary>
-    // /// 5:一元调用:交易对配置更改
-    // /// </summary>
-    // /// <param name="request"></param>
-    // /// <param name="context"></param>
-    // /// <returns></returns>
-    // public override async Task<ConfigSymbolRes> ConfigSymbol(ConfigSymbolReq request, ServerCallContext context)
-    // {
-    //     ConfigSymbolRes res = new ConfigSymbolRes();
-    //     res.Success = true;
-    //     return await Task.FromResult(res);
-    // }
-
-    // /// <summary>
-    // /// 6:服务器流:成交记录
-    // /// </summary>
-    // /// <param name="request"></param>
-    // /// <param name="context"></param>
-    // /// <returns></returns>
-    // public override async Task TransactionRecord(TransactionRecordReq request, IServerStreamWriter<TransactionRecordRes> responseStream, ServerCallContext context)
-    // {
-    //     FactoryTrade.instance.Channel_TransactionRecord = System.Threading.Channels.Channel.CreateUnbounded<TransactionRecordRes>();
-    //     Task.Run(async () =>
-    //     {
-    //         await foreach (var message in FactoryTrade.instance.Channel_TransactionRecord.Reader.ReadAllAsync())
-    //         {
-    //             await responseStream.WriteAsync(message);
-    //         }
-    //     }).Start();
-
-
-
-    //     long sysmbol_id = request.SymbolId;
-    //     // if (consumerTask.IsCompleted)
-    //     Task.Run(async () =>
-    //     {
-    //         await Task.Delay(3000);
-    //         for (int i = 0; i < 10000; i++)
-    //         {
-    //             TransactionRecordRes res = new TransactionRecordRes();
-    //             res.Record = sysmbol_id + 1;
-    //             await FactoryTrade.instance.Channel_TransactionRecord.Writer.WriteAsync(res);
-    //             await Task.Delay(100);
-    //         }
-
-    //     }).Start();
-
-    //     await Task.Delay(100);
-    //     // channel.Writer.Complete();
-    // }
-
-
 
 }
